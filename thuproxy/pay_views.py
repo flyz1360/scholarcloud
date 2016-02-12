0c139731523a3523867be4117507bf6a1128e27c
flyz1360 <luzhen02@gmail.com>
2016-02-12 17:15:06 +0800

    去掉内测支付后台验证

--- thuproxy/pay_views.py
+++ thuproxy/pay_views.py
@@ -146,10 +146,7 @@ def alipay_callback(request):
                 print ('paytype', pay_type)
                 print ('month',month)
                 # todo
-                if float(total_fee) == 0.10:
-                    real_fee = float(total_fee) * 10
-                else:
-                    real_fee = float(total_fee)*100
+                real_fee = float(total_fee)*100
                 print ('realfee',real_fee)
 
                 if pay_type == 1:
