2cdf0fb2a7ebd408a50a49f485676fe0eca13af6
flyz1360 <luzhen02@gmail.com>
2016-04-19 22:19:19 +0800

    修改json返回

--- thuproxy/tests.py
+++ thuproxy/tests.py
@@ -1,8 +1,20 @@
-#coding=utf-8
+#encoding=utf-8
 import urllib
 import urllib.request
 import json
-from django.utils import timezone
+import sys
 
-now = timezone.now()
-print(now)
+try:
+    url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?'
+    url_values = urllib.parse.urlencode({'format':'js-8', 'ip':'106.37.225.179'})
+    full_url = url+url_values
+    # print(full_url)
+    ip_data = urllib.request.urlopen(full_url).read()
+    ip_data_unicode = ip_data.decode('unicode_escape')
+    ip_data_unicode = ip_data_unicode[21:len(ip_data_unicode)-1]
+    print(ip_data_unicode)
+    result = json.loads(ip_data_unicode)
+    print(result)
+    print(result['city'])
+except Exception as e:
+    print(e)
